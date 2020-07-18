7cc079b6c92d9656527de18cf3d1b313bc22ec90
norberto <norberto.a.vega@gmail.com>
2020-07-18 03:06:48 -0300

    Agrega Dao(VideoDao) y la Room database(VideosDatabase) que se con la funcion getDatabase()

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -1,1 +1,32 @@
 package com.example.android.devbyteviewer.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface VideoDao {
+
+    @Query("select * from databasevideo")
+    fun getVideos(): LiveData<List<DatabaseVideo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg videos: DatabaseVideo)
+}
+
+
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class Videosdatabase: RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: Videosdatabase
+
+fun getDatabase(context: Context): Videosdatabase {
+    synchronized(Videosdatabase::class.java) {
+        if (!::INSTANCE.isInitialized)
+            INSTANCE = Room.databaseBuilder(context.applicationContext, Videosdatabase::class.java, "videos").build()
+    }
+    return INSTANCE
+}
